f4ecf3a92cad29990b0cca0b5c030498f6a7c995
mani chandana <nmcreddy2000@gmail.com>
2024-12-07 18:00:49 +0530

    feat: divisible by 400 is a leap year

--- leapYear.js
+++ leapYear.js
@@ -1,6 +1,5 @@
-// leapYear.js
 function isLeapYear(year) {
-    return year % 4 === 0 && year % 100 !== 0;
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 }
 
 module.exports = isLeapYear;
